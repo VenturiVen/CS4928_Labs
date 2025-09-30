37e68c8634231455c273b50b0bf00ba9ccc95953
Aron Calvert <calvertaron@gmail.com>
2025-09-30 17:48:08 +0100

    indentation yay

--- cafe-pos-week3/src/test/java/com/cafepos/OrderPaymentTests.java
+++ cafe-pos-week3/src/test/java/com/cafepos/OrderPaymentTests.java
@@ -20,13 +20,13 @@ public class OrderPaymentTests {
 
     @Test 
     void payment_strategy_called() {
-    var p = new SimpleProduct("A", "A", Money.of(5));
-    var order = new Order(42);
-    order.addItem(new LineItem(p, 1));
-    final boolean[] called = {false};
-    PaymentStrategy fake = o -> called[0] = true;
-    order.pay(fake);
-    assertTrue(called[0], "Payment strategy should be called");
+        var p = new SimpleProduct("A", "A", Money.of(5));
+        var order = new Order(42);
+        order.addItem(new LineItem(p, 1));
+        final boolean[] called = {false};
+        PaymentStrategy fake = o -> called[0] = true;
+        order.pay(fake);
+        assertTrue(called[0], "Payment strategy should be called");
     }
 
 
