393f35d4245c2fc46c09e8f6e713e322669ab2c9
Aron Calvert <calvertaron@gmail.com>
2025-09-30 16:47:24 +0100

    little change

--- cafe-pos-week3/src/main/java/com/cafepos/domain/CustomerNotifier.java
+++ cafe-pos-week3/src/main/java/com/cafepos/domain/CustomerNotifier.java
@@ -3,8 +3,7 @@ package com.cafepos.domain;
 public final class CustomerNotifier implements OrderObserver {
 @Override
 public void updated(Order order, String eventType) {
-// TODO: print "[Customer] Dear customer, your Order #<id> has been updated: <event>."
-
-    System.out.println("[Customer] Dear customer, your Order #" + order.id() + " has been updated: " + eventType + ".");
+    System.out.println("[Customer] Dear customer, your Order #" + order.id() 
+                        + " has been updated: " + eventType + ".");
 }
 }
