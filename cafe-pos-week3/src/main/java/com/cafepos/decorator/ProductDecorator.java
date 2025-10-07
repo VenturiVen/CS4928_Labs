a732959af06c1b525d99651afa2a5e26b6ade1c0
VenturiVen <gingeroshea443@gmail.com>
2025-10-07 16:29:32 +0100

    ProductFactory.java, wk5 demo

--- cafe-pos-week3/src/main/java/com/cafepos/decorator/ProductDecorator.java
+++ cafe-pos-week3/src/main/java/com/cafepos/decorator/ProductDecorator.java
@@ -1,4 +1,5 @@
 package com.cafepos.decorator;
+
 import com.cafepos.catalog.Product;
 import com.cafepos.common.Money;
 
@@ -6,17 +7,22 @@ public abstract class ProductDecorator implements Product {
     protected final Product base;
 
     protected ProductDecorator(Product base) {
-        if (base == null) throw new IllegalArgumentException("base product required");
+        if (base == null)
+            throw new IllegalArgumentException("base product required");
         this.base = base;
     }
 
-    @Override public String id() { return base.id(); 
-    // id may remain the base product id
+    @Override
+    public String id() {
+        return base.id();
+        // id may remain the base product id
     }
-        
-    @Override public Money basePrice() { return
-    base.basePrice(); 
+
+    @Override
+    public Money basePrice() {
+        return base.basePrice();
     } // original price (not total)
-    
-    // Concrete decorators will override name() and provide a finalPrice() helper if desired.
+
+    // Concrete decorators will override name() and provide a finalPrice() helper if
+    // desired.
 }
